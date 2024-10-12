ed373b2f6ea55fc616aabe6f46e2bb1eb0b161ed
Lucas <lucas.ribeiro711@gmail.com>
2024-10-11 22:33:32 -0300

    movendo os comentários para o arquivo C

--- aula_01.c
+++ aula_01.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 // Variáveis
+// Diferente de algumas linguagens que são tipadas dinamicamente o C você precisa inferir o tipo da variável na sua declaração, sendo a sua declaração
 
-int inteiro = 5;
-float quebrado = 5.8;
-double precisao = 5.8;
-char caracter = 'c';
+int inteiro = 5;       // int para declarar uma variável do tipo numero inteiro
+float quebrado = 5.8;  // float para declarar uma variável do tipo numero float(quebrado)
+double precisao = 5.8; // float para declarar uma variável do tipo numero double(tem mais precisão no seu valor quebrado)
+char caracter = 'c';   // char para declarar uma variável do tipo carácter (recebe um carácter)
 
 int main(void)
 {
@@ -14,3 +15,5 @@ int main(void)
   printf("o valor da variável 'carácter' é %c\n", caracter);
   return 0;
 }
+
+// Ao final de cada linha do código é obrigatório conter um < ; > para sinalizar o final da linha
